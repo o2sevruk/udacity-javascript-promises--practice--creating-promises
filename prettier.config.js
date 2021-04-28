8cf76c2c9533f1d876fb99143067880591efab3f
o2sevruk <o2sevruk@gmail.com>
2021-04-28 13:36:55 +0300

    Environment set up.

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  overrides: [
+    {
+      files: '*.js',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
+};
